563ba5382641b15268915bdbf5fb3cb856639a05
avinaashacelema <avinaash@acelema.com>
2026-05-18 07:41:19 +0530

    Fix Next config and trigger rebuild

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ['ckeditor5', '@ckeditor/ckeditor5-react'],
+  allowedDevOrigins: ['169.254.51.5'],
 };
 
 export default nextConfig;
